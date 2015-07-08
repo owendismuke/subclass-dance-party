44fef5a1932bf41a8892cc904cc23ae86b8e0e58
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-08 08:48:35 -0700

    .

--- src/init.js
+++ src/init.js
@@ -53,36 +53,38 @@ $(document).ready(function(){
   //We can not get to work -_-
 
   // Math.floor(Math.random() * (window.dancers.length - 1))
-  // var updateDancers = function(){
+  var updateDancers = function(){
 
-  //   var arr = [];
-  //   for(var i = 0; i < window.dancers.length; i++){
-  //     var closestDancer = window.dancers.reduce(function(prev, curr){
-  //       //get distance from window.dancer[i] on current
-  //       var currentDistance = Math.sqrt(Math.pow(curr.top - window.dancers[i].top, 2) + Math.pow(curr.left - window.dancers[i].left, 2));
-  //       //if current closer than previous
-  //       if(prev){
-  //         if(prev[0] < currentDistance){
-  //           return prev;
-  //         }
-  //       }
+    var arr = [];
+    for(var i = 0; i < window.dancers.length; i++){
+      var closestDancer = window.dancers.reduce(function(prev, curr){
+        //get distance from window.dancer[i] on current
+        var currentDistance = Math.sqrt(Math.pow(curr.top - window.dancers[i].top, 2) + Math.pow(curr.left - window.dancers[i].left, 2));
+        //if current closer than previous
+        if(prev){
+          if(prev[0] < currentDistance){
+            return prev;
+          }
+        }
 
-  //       return [currentDistance, curr];
-  //     });
+        return [currentDistance, curr];
+      });
 
-  //   //move or something to that dancer
-  //     //halve the lefts
-  //     var halfLeft = window.dancers[i].left > closestDancer[1].left ? window.dancers[i].left - closestDancer[1].left : closestDancer[1].left - window.dancers[i].left;
-  //     //halve the tops to get the middle
-  //     var halfTop = window.dancers[i].top > closestDancer[1].left ? window.dancers[i].top - closestDancer[1].top : closestDancer[1].top - window.dancers[i].top;
-  //     //setPosition window dancer on halves for top and left
-  //     window.dancers[i].setPosition(halfTop, halfLeft);
-  //     //setposition closest dancer on half for top and half left + 100
-  //     closestDancer[1].setPosition(halfTop, halfLeft + 100);
-  //   }
-  // };
+    //move or something to that dancer
+      //halve the lefts
+      //var halfLeft = window.dancers[i].left > closestDancer[1].left ? window.dancers[i].left - closestDancer[1].left : closestDancer[1].left - window.dancers[i].left;
+      var halfLeft = (window.dancers[i].left + closestDancer[1].left) / 2;
+      //halve the tops to get the middle
+      //var halfTop = window.dancers[i].top > closestDancer[1].left ? window.dancers[i].top - closestDancer[1].top : closestDancer[1].top - window.dancers[i].top;
+      var halfTop = (window.dancers[i].top + closestDancer[1].top) / 2;
+      //setPosition window dancer on halves for top and left
+      window.dancers[i].setPosition(halfTop, halfLeft);
+      //setposition closest dancer on half for top and half left + 100
+      closestDancer[1].setPosition(halfTop, halfLeft + 100);
+    }
+  };
 
-  // setInterval(updateDancers, 5000);
+  setInterval(updateDancers, 5000);
 
 });
 
